10208b36a9ae52adfa2b3e1b48f838177187b597
Stephen Contreras <stephenjcontreras@gmail.com>
2020-05-22 23:54:30 -0500

    Updates

---
+++ precache-manifest.3609da0604746c861ecd46538ef6b522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debcde22dc460ebf782c18bffd835d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a287524101cce04a7036",
+    "url": "/static/css/2.b0a67369.chunk.css"
+  },
+  {
+    "revision": "ab83f4588ea38ebe700b",
+    "url": "/static/css/main.9f4cb080.chunk.css"
+  },
+  {
+    "revision": "a287524101cce04a7036",
+    "url": "/static/js/2.86ad8ccb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.86ad8ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab83f4588ea38ebe700b",
+    "url": "/static/js/main.b99fb031.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  }
+]);
